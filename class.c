c378e890a04a1ce4587a87af3c7e269f35335b5b
Devansh Verma <vermadevansh0001@gmail.com>
2025-01-30 21:10:52 +0530

    added

--- class.c
+++ class.c
@@ -8,14 +8,29 @@ int main(){
     // }
     // while(i>=0);
     //bitwise
-    int a,b;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
+    // int a,b;
+    // printf("Enter the value of a:");
+    // scanf("%d",&a);
 
-    printf("Enter the value of b:");
-    scanf("%d",&b);
+    // printf("Enter the value of b:");
+    // scanf("%d",&b);
 
-    printf("%d",a&b);
+    // printf("%d",a&b);
+
+    int num, result, shift;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Enter the number of positions to shift: ");
+    scanf("%d", &shift);
+
+    result = num >> shift;
+
+    printf("Result of %d right-shifted by %d positions: %d\n", num, shift, result);
+
+    return 0;
+}
+
 
     
-}
