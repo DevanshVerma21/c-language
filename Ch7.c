69f3b9169662819ebd288bdc76ee73400b1fbc8b
Devansh Verma <vermadevansh0001@gmail.com>
2025-03-04 18:29:17 +0530

    added

--- Ch7.c
+++ Ch7.c
@@ -1,9 +1,65 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 
+// void language(){
+//     char str[100];
+//     scanf("%s", str);
+//     if (strcmp(str, "India")==0){
+//         printf("Namaste");
+//     }
+//     else if(strcmp(str, "French")== 0){
+//         printf("Bonjour");
+//     }
+//     else{
+//          printf("Error");
+//     }
+// }
 
+// int sum(int a, int b);
+// void printingtable(int n);
 
+// int main(){
+//     // language();
+//     int n;
+//     scanf("%d",&n);
+//     printingtable(n);//actual parameter
 
-int main(){
-    return 0;
+//     return 0;
+// }
+
+// int sum (int x, int y){
+//     return x + y;
+// }
+
+// void printingtable(int n){//formal parameter
+//     for (int i = 1 ; i<=10;i++){
+//         printf("%d\n", n * i );
+//     }
+// }
+
+// void calculatePrice(float value);
+
+// int main(){
+//     float value = 100.0;
+//     calculatePrice(value);
+//     return 0;
+// }
+
+// void calculatePrice(float value){
+//     value = value + (0.18 * value);
+//     printf("final price is %f",value);
+// }
+
+void square(float number);
+
+int main(float number){
+    square(number);
+}
+
+void square(float number){
+    scanf("%f",&number);
+    float result = pow(number,2);
+    printf("%f", result);
 }
