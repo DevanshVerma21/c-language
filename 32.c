5b338c713f8be734246425b65c806940e4177d7c
Devansh Verma <vermadevansh0001@gmail.com>
2025-04-01 03:10:59 +0530

    more codes

--- 32.c
+++ 32.c
@@ -1,6 +1,16 @@
 //WAP in C to print prime numbers only from an array
 #include <stdio.h>
-
+int isprime(int num){
+    if(num<=1){
+        return 0;
+    }
+    for(int i = 2 ; i*i<=num;i++){
+        if(num % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
 int main(){
     int n;
     printf("Enter the values of n:");
@@ -9,8 +19,14 @@ int main(){
     int arr[n];
     printf("values:\n");
     for(int i =0;i<n;i++){
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
 
-    
+    printf("Prime Values:\n");
+    for(int i =0;i<n;i++){
+        if (isprime(arr[i])){
+            printf("%d ",arr[i]);
+        }
+    }
+    printf("\n");
 }
